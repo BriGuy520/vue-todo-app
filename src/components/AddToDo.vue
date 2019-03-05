e180992a640758eaa651f874c81d89a0e25846d9
Brian Falasz <brianfalasz@gmail.com>
2019-03-04 23:52:31 -0600

    small todo-app made with vue js

---
+++ src/components/AddToDo.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <form @submit="addToDo">
+      <input type="text" v-model="title" name="title" placeholder="Add Todo...">
+      <input type="submit" value="Submit" class="btn">
+    </form>
+  </div>
+</template>
+
+
+<script>
+// import uuid from 'uuid';
+export default {
+  name: "AddToDo",
+  data(){
+    return {
+      title: ''
+    }
+  },
+  methods: {
+    addToDo(e){
+      e.preventDefault();
+
+      const newToDo = {
+        title: this.title,
+        completed: false
+      }
+      // Send up to parent
+      this.$emit('add-todo', newToDo);
+    }
+  }
+}
+</script>
+
+<style scoped>
+  form {
+    display: flex;
+  }
+
+  input[type="text"] {
+    flex: 10;
+    padding: 5px;
+  }
+
+  input[type="submit"]{
+    flex: 2;
+  }
+</style>
+
+
